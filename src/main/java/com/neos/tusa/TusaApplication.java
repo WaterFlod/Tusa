70506fb39fac3b037eebbac52d27a5ce842bbb23
Shestakov Matvey <mse25019@gmail.com>
2026-07-14 22:39:15 +0500

    init project

---
+++ src/main/java/com/neos/tusa/TusaApplication.java
@@ -0,0 +1,13 @@
+package com.neos.tusa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TusaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TusaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neos/tusa/TusaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neos.tusa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TusaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
