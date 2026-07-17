08a9be044c98cd8d43df13b7a7ba7d7f1a3757e3
Shestakov Matvey <mse25019@gmail.com>
2026-07-17 13:26:01 +0500

    feat: add PartyRepository

---
+++ src/main/java/com/neos/tusa/PartyRepository.java
@@ -0,0 +1,9 @@
+package com.neos.tusa;
+
+import com.neos.tusa.model.Party;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PartyRepository extends JpaRepository<Party, Long> {
+}
