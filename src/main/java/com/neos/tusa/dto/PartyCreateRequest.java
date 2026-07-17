3c9078182fc337db4a1b3f6dadaf563f84e1a8b9
Shestakov Matvey <mse25019@gmail.com>
2026-07-17 21:46:59 +0500

    fix: replace @NotBlank with @NotNull

--- src/main/java/com/neos/tusa/dto/PartyCreateRequest.java
+++ src/main/java/com/neos/tusa/dto/PartyCreateRequest.java
@@ -1,11 +1,12 @@
 package com.neos.tusa.dto;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record PartyCreateRequest(
         @NotBlank(message = "The name cannot be empty")
         String name,
-        @NotBlank(message = "Admin must be specified")
+        @NotNull(message = "Admin must be specified")
         Long adminUserId
 ) {
 }
