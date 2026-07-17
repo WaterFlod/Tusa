6f35070df77c70a242bb3d597f185afa7972f0d2
Shestakov Matvey <mse25019@gmail.com>
2026-07-17 13:27:04 +0500

    feat: add BillRepository

---
+++ src/main/java/com/neos/tusa/BillRepository.java
@@ -0,0 +1,9 @@
+package com.neos.tusa;
+
+import com.neos.tusa.model.Bill;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BillRepository extends JpaRepository<Bill, Long> {
+}
