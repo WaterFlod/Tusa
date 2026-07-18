c29534d732fd78c61532eb2cace453676694586e
Shestakov Matvey <mse25019@gmail.com>
2026-07-18 23:58:49 +0500

    feat: add getter and setter in Bill

--- src/main/java/com/neos/tusa/model/Bill.java
+++ src/main/java/com/neos/tusa/model/Bill.java
@@ -1,16 +1,22 @@
 package com.neos.tusa.model;
 
 import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "bill")
 @NoArgsConstructor
+@Setter
+@Getter
 public class Bill {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
     private long id;
 
     @Column(nullable = false)
