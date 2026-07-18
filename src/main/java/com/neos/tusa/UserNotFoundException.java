e1f5d1b7e0eabcd224b3f363db987f152f51ca37
Shestakov Matvey <mse25019@gmail.com>
2026-07-19 00:22:02 +0500

    feat: update UserNotFoundException
    - add userId
    - add Getter
    - add Constructor with message

--- src/main/java/com/neos/tusa/UserNotFoundException.java
+++ src/main/java/com/neos/tusa/UserNotFoundException.java
@@ -1,7 +1,24 @@
 package com.neos.tusa;
 
+import lombok.Getter;
+
+@Getter
 public class UserNotFoundException extends RuntimeException {
-    public UserNotFoundException(String message) {
+
+    private final Long userId;
+
+    public UserNotFoundException(Long userId) {
+        super(String.format("User with this %d id was not found", userId));
+        this.userId = userId;
+    }
+
+    public UserNotFoundException(String message, Long userId) {
         super(message);
+        this.userId = userId;
+    }
+
+    public UserNotFoundException(String message, Throwable cause, Long userId) {
+        super(message, cause);
+        this.userId = userId;
     }
 }
