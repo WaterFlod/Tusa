af5d6b289985b0ca74e3801a82a6edceb6acb3f7
Shestakov Matvey <mse25019@gmail.com>
2026-07-18 23:59:56 +0500

    feat: add method existsUserInParty

--- src/main/java/com/neos/tusa/repository/PartyRepository.java
+++ src/main/java/com/neos/tusa/repository/PartyRepository.java
@@ -2,8 +2,16 @@ package com.neos.tusa.repository;
 
 import com.neos.tusa.model.Party;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PartyRepository extends JpaRepository<Party, Long> {
+
+    @Query("SELECT CASE WHEN COUNT(p) > 0 THEN true ELSE false END " +
+           "FROM Party p JOIN p.members m " +
+           "WHERE p.id = :partyId AND m.id = :userId")
+    boolean existsUserInParty(@Param("userId") Long userId,
+                              @Param("partyId") Long partyId);
 }
