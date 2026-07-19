3befced7ed5b585d0122b14899c19694c1235b04
Shestakov Matvey <mse25019@gmail.com>
2026-07-19 20:41:30 +0500

    feat: add a notification when creating a new Bill

--- src/main/java/com/neos/tusa/BillService.java
+++ src/main/java/com/neos/tusa/BillService.java
@@ -5,12 +5,17 @@ import com.neos.tusa.exception.PartyNotFoundException;
 import com.neos.tusa.exception.UserNotFoundException;
 import com.neos.tusa.exception.UserNotInPartyException;
 import com.neos.tusa.model.Bill;
+import com.neos.tusa.model.Party;
+import com.neos.tusa.model.User;
 import com.neos.tusa.repository.BillRepository;
 import com.neos.tusa.repository.PartyRepository;
 import com.neos.tusa.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +24,7 @@ public class BillService {
     private final BillRepository billRepository;
     private final UserRepository userRepository;
     private final PartyRepository partyRepository;
+    private final TusaBot bot;
 
     @Transactional
     public Bill createBill(AddBillRequest request) {
@@ -34,6 +40,20 @@ public class BillService {
         bill.setUser(userRepository.getReferenceById(request.userId()));
         bill.setParty(partyRepository.getReferenceById(request.partyId()));
 
-        return billRepository.save(bill);
+        bill = billRepository.save(bill);
+
+        Party party = partyRepository.findById(request.partyId()).orElseThrow(() -> new PartyNotFoundException(request.partyId()));
+        List<User> members = party.getMembers();
+        for (User user : members) {
+            bot.sendMessage(user.getChatId(),
+                    String.format(
+                            "Добавлен новый счет на сумму %s руб. \nОписание:%s",
+                            bill.getAmount().toString(),
+                            bill.getDescription()
+                    )
+            );
+        }
+
+        return bill;
     }
 }
